6a09e4d7bb70d3946ece947527fbe5bc6ceb138a
Robert <rnguyen350@gmail.com>
2022-07-05 08:03:13 -0400

    accidentally deleted datetime converter

--- app/src/main/java/com/hfad/guineapiglog/Converter.kt
+++ app/src/main/java/com/hfad/guineapiglog/Converter.kt
@@ -4,7 +4,7 @@ import androidx.room.TypeConverter
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
-object                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  Converter {
+object Converter {
     private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
     @TypeConverter
