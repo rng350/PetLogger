bdf22c44a6fb90ac94f729de36f19b4b83933cf2
Robert <rnguyen350@gmail.com>
2024-06-09 17:28:27 -0400

    added permission requests to mediaselection screen

--- app/src/main/java/com/hfad/petlogger/MediaSelectionFragment.kt
+++ app/src/main/java/com/hfad/petlogger/MediaSelectionFragment.kt
@@ -1,5 +1,7 @@
 package com.hfad.petlogger
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -10,6 +12,8 @@ import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.viewModels
 import com.hfad.petlogger.databinding.FragmentMediaSelectionBinding
 import com.hfad.petlogger.photoselection.AdjustablePickMultipleVisualMedia
@@ -19,6 +23,10 @@ class MediaSelectionFragment : Fragment() {
     private var _binding: FragmentMediaSelectionBinding? = null
     private val binding get() = _binding!!
     private val viewModel: MediaSelectionViewModel by viewModels({requireParentFragment()})
+
+    private lateinit var multiPickupVisualMediaContract: AdjustablePickMultipleVisualMedia
+    private lateinit var pickMultipleMedia: ActivityResultLauncher<PickVisualMediaRequest>
+    private lateinit var pickSingleMedia: ActivityResultLauncher<PickVisualMediaRequest>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,37 +45,72 @@ class MediaSelectionFragment : Fragment() {
             photoToggle = { photo -> viewModel.removePhotoFromSelection(photo) }
         )()
 
-        val multiPickupVisualMediaContract = AdjustablePickMultipleVisualMedia(viewModel.maxItems - (viewModel.currentPhotoSelection.value?.size ?: 0))
+        multiPickupVisualMediaContract = AdjustablePickMultipleVisualMedia(viewModel.maxItems - (viewModel.currentPhotoSelection.value?.size ?: 0))
 
-        val pickMultipleMedia =
+        pickMultipleMedia =
             registerForActivityResult(multiPickupVisualMediaContract) { uris ->
                 if (uris.isNotEmpty()) {
                     viewModel.retrievePhotoSelection(requireContext(), uris)
                 }
             }
 
-        val pickSingleMedia =
+        pickSingleMedia =
             registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
                 uri?.let {
                     viewModel.retrievePhotoSelection(requireContext(), listOf(uri))
                 }
             }
 
-        binding.addPhotosButton.setOnClickListener {
-            val maxItems = viewModel.maxItems - (viewModel.currentPhotoSelection.value?.size ?: 0)
-            if (maxItems > 1) {
-                multiPickupVisualMediaContract.updateMaxItems(maxItems)
-                pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-            }
-            else if (maxItems == 1) {
-                pickSingleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        val requestPermissions = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+                results ->
+            if (        (Build.VERSION.SDK_INT>= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+                        && results[Manifest.permission.READ_MEDIA_IMAGES] == true
+                        && results[Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED] == true)
+
+                ||      (Build.VERSION.SDK_INT>= Build.VERSION_CODES.TIRAMISU
+                        && results[Manifest.permission.READ_MEDIA_IMAGES] == true)
+
+                ||      (results[Manifest.permission.READ_EXTERNAL_STORAGE] == true)
+            ) {
+                launchImagePicker()
             }
             else {
-                Toast.makeText(
-                    requireContext(),
-                    getString(R.string.photo_selection_limit_reached),
-                    Toast.LENGTH_SHORT
-                ).show()
+                makePermissionsRequiredToast()
+            }
+        }
+
+        binding.addPhotosButton.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+                if ((ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+                    ) == PermissionChecker.PERMISSION_GRANTED)
+                    || (ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    ) == PermissionChecker.PERMISSION_GRANTED)) {
+                    launchImagePicker()
+                }
+                else requestPermissions.launch(arrayOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+                ))
+            }
+            else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                if (ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    ) == PermissionChecker.PERMISSION_GRANTED
+                ) {
+                    launchImagePicker()
+                }
+                else requestPermissions.launch(arrayOf(Manifest.permission.READ_MEDIA_IMAGES))
+            }
+            else {
+                if (ContextCompat.checkSelfPermission(requireContext(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    ) == PermissionChecker.PERMISSION_GRANTED
+                ) {
+                    launchImagePicker()
+                }
+                else requestPermissions.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
             }
         }
 
@@ -81,5 +124,27 @@ class MediaSelectionFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun launchImagePicker() {
+        val maxItems = viewModel.maxItems - (viewModel.currentPhotoSelection.value?.size ?: 0)
+        if (maxItems > 1) {
+            multiPickupVisualMediaContract.updateMaxItems(maxItems)
+            pickMultipleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        else if (maxItems == 1) {
+            pickSingleMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        else {
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.photo_selection_limit_reached),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun makePermissionsRequiredToast() {
+        Toast.makeText(requireContext(), "Can't read files without permission.", Toast.LENGTH_LONG).show()
     }
 }
